852c5c81181167a4b1cbb6513560537869f9f826
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-04-05 19:16:47 +0530

    Add files via upload

---
+++ Lab 7 : Arithmetic Circuits and ALU/graph.py
@@ -0,0 +1,20 @@
+
+# importing the required module
+import matplotlib.pyplot as plt
+  
+# x axis values - pl
+x = [8,16]
+# corresponding y axis values - time
+y = [15.378,22.047]
+  
+# plotting the points 
+plt.plot(x, y)
+  
+# plt.xlabel('BIT-WIDTH')
+# plt.ylabel('DELAY')
+  
+# giving a title to my graph
+# plt.title('BIT-WIDTH v/s DELAY')
+  
+# function to show the plot
+plt.show()
